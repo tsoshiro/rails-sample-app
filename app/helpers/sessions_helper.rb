01ee7412ef259a403489e085be500133624fde68
oshiro <tsubasa.oshiro@gmail.com>
2018-07-25 17:21:15 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,12 +10,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
-
-  
+  #  Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      # raise # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
