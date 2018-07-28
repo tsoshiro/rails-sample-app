8711560002cbd3f7e5253a42410820382601915c
oshiro <tsubasa.oshiro@gmail.com>
2018-07-28 10:30:10 +0000

    Fix bugs: users_path and user_path

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,9 +7,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "form[action=?]", signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: {name: "",
-                                        email: "user@invalid",
-                                        password: "foo",
-                                        password_confirmation: "bar"} }
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"} }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
