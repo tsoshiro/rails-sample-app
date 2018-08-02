c726f5cfb518b48167f5c74f7cc2e4f42bd6da26
oshiro <tsubasa.oshiro@gmail.com>
2018-08-02 17:05:09 +0000

    Image uploader implementing. But test doesn't go green somehow

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -16,7 +16,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # invalid submission
     assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: 'a' * 141 } }
+      post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
     
@@ -40,5 +40,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @other = users(:archer)
     get user_path(@other)
     assert_select  'a', text: 'delete', count: 0
+  end
+  
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
