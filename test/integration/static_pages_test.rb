2039065e5f3b014c0e0accf23221a9875b6aab2d
oshiro <tsubasa.oshiro@gmail.com>
2018-08-06 14:59:34 +0000

    Working on following/followers

---
+++ test/integration/static_pages_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class StaticPagesTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+  
+  def setup
+    @user = users(:michael)
+  end
+    
+  test "show home" do
+    log_in_as(@user)
+    get root_path
+    
+    assert_template 'static_pages/home'
+    # assert_select 'title', full_title(@user.name)
+    
+    assert_select 'h1', text: @user.name
+    assert_select 'img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body 
+    assert_select 'div.pagination', count: 1
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+    
+    assert_select 'strong#following', "#{@user.following.count}"
+    assert_select 'strong#followers', "#{@user.followers.count}"
+  end
+end
